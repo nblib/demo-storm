4d92aa2d907f59d4c662c8a8a7511a3c26f52a1b
hewe <272489473@qq.com>
2017-12-06 10:54:42 +0800

    modify clutser

--- src/main/java/demo/hewe/Application.java
+++ src/main/java/demo/hewe/Application.java
@@ -4,7 +4,10 @@ import demo.hewe.bolts.WordCounter;
 import demo.hewe.bolts.WordNormalizer;
 import demo.hewe.spouts.WordReader;
 import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
 
@@ -19,17 +22,28 @@ public class Application {
         topologyBuilder.setBolt("word-counter", new WordCounter(), 2).fieldsGrouping("word-normalizer", new Fields("word"));
 
         Config config = new Config();
-        config.put("wordsFile", "D:/words.txt");
+        config.put("wordsFile", args[0]);
         config.setDebug(false);
         config.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
-
-        LocalCluster localCluster = new LocalCluster();
+        //本地运行模式
+       /* LocalCluster localCluster = new LocalCluster();
         localCluster.submitTopology("Getting-Started-Toplogie", config, topologyBuilder.createTopology());
         try {
             Thread.sleep(50000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        localCluster.shutdown();
+        localCluster.shutdown();*/
+        //集群运行模式
+        try {
+            StormSubmitter.submitTopology("Count-Word-Topology-With-Refresh-Cache", config,
+                    topologyBuilder.createTopology());
+        } catch (AlreadyAliveException e) {
+            e.printStackTrace();
+        } catch (InvalidTopologyException e) {
+            e.printStackTrace();
+        } catch (AuthorizationException e) {
+            e.printStackTrace();
+        }
     }
 }
